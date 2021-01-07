06ab653634e3905f9c3701713362f7b645e9d23d
Luthfi Arifin <luthfiarifin0222@gmail.com>
2021-01-07 23:18:32 +0700

    Change val to var

--- core/src/main/java/com/laam/core/data/source/local/entity/NewsEntity.kt
+++ core/src/main/java/com/laam/core/data/source/local/entity/NewsEntity.kt
@@ -10,16 +10,16 @@ import com.laam.core.data.source.local.room.NewsDatabase
  */
 @Entity(tableName = NewsDatabase.NEWS_COLUMN_NAME)
 data class NewsEntity(
-    @ColumnInfo(name = "author") val author: String,
-    @ColumnInfo(name = "content") val content: String?,
-    @ColumnInfo(name = "description") val description: String,
-    @ColumnInfo(name = "publishedAt") val publishedAt: String,
-    @ColumnInfo(name = "source") val source: String,
-    @ColumnInfo(name = "title") val title: String,
-    @ColumnInfo(name = "url") val url: String,
-    @ColumnInfo(name = "urlToImage") val urlToImage: String
+    @ColumnInfo(name = "author") var author: String,
+    @ColumnInfo(name = "content") var content: String?,
+    @ColumnInfo(name = "description") var description: String,
+    @ColumnInfo(name = "publishedAt") var publishedAt: String,
+    @ColumnInfo(name = "source") var source: String,
+    @ColumnInfo(name = "title") var title: String,
+    @ColumnInfo(name = "url") var url: String,
+    @ColumnInfo(name = "urlToImage") var urlToImage: String
 ) {
 
     @PrimaryKey(autoGenerate = true)
-    @ColumnInfo(name = "id") val newsId: Long? = null
+    @ColumnInfo(name = "id") var newsId: Long? = null
 }
