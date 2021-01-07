14355b29a397ae9c57c6c8a1ea9af49d4c3eb0d1
Luthfi Arifin <luthfiarifin0222@gmail.com>
2021-01-07 18:06:59 +0700

    Add content string nullable

--- core/src/main/java/com/laam/core/data/source/remote/response/ArticleResponse.kt
+++ core/src/main/java/com/laam/core/data/source/remote/response/ArticleResponse.kt
@@ -7,7 +7,7 @@ import com.google.gson.annotations.SerializedName
  */
 data class ArticleResponse(
    @SerializedName("author") val author: String,
-   @SerializedName("content") val content: String,
+   @SerializedName("content") val content: String?,
    @SerializedName("description") val description: String,
    @SerializedName("publishedAt") val publishedAt: String,
    @SerializedName("source") val source: SourceResponse,
