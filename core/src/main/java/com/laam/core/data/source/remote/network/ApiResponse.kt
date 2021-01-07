275a16355984ed7181dc466f91ae6b752369fc61
Luthfi Arifin <luthfiarifin0222@gmail.com>
2021-01-07 17:51:48 +0700

    Create sealed `ApiResponse` class

---
+++ core/src/main/java/com/laam/core/data/source/remote/network/ApiResponse.kt
@@ -0,0 +1,12 @@
+package com.laam.core.data.source.remote.network
+
+/**
+ * Created by luthfiarifin on 1/7/2021.
+ */
+sealed class ApiResponse<out R> {
+    data class Success<out T>(val data: T) : ApiResponse<T>()
+
+    data class Error(val errorMessage: String) : ApiResponse<Nothing>()
+
+    object Empty : ApiResponse<Nothing>()
+}
