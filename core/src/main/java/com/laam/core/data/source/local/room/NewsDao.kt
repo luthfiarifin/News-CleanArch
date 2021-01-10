8cfe842dbd4ed8439a52ca4cc352e1e76fa9cf67
Luthfi Arifin <luthfiarifin0222@gmail.com>
2021-01-10 18:14:22 +0700

    Create & configure NewsDao.kt

---
+++ core/src/main/java/com/laam/core/data/source/local/room/NewsDao.kt
@@ -0,0 +1,18 @@
+package com.laam.core.data.source.local.room
+
+import androidx.room.*
+import com.laam.core.data.source.local.entity.NewsEntity
+import kotlinx.coroutines.flow.Flow
+
+/**
+ * Created by luthfiarifin on 1/9/2021.
+ */
+@Dao
+interface NewsDao {
+
+    @Query("SELECT * FROM ${NewsDatabase.NEWS_COLUMN_NAME}")
+    fun getAllNews(): Flow<List<NewsEntity>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertNews(newsEntity: List<NewsEntity>)
+}
