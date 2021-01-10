f9b012d830d9fb424030d6b901eeb288005dbcc1
Luthfi Arifin <luthfiarifin0222@gmail.com>
2021-01-10 19:27:45 +0700

    Setup `TopHeadlineAdapter`

--- feature/home/src/main/java/com/laam/home/home/HomeFragment.kt
+++ feature/home/src/main/java/com/laam/home/home/HomeFragment.kt
@@ -2,7 +2,9 @@ package com.laam.home.home
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import com.laam.base.BaseFragment
+import com.laam.core.presentation.model.News
 import com.laam.home.R
 import com.laam.home.databinding.FragmentHomeBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -12,11 +14,30 @@ import dagger.hilt.android.AndroidEntryPoint
  */
 @AndroidEntryPoint
 class HomeFragment : BaseFragment<FragmentHomeBinding, HomeViewModel>() {
+
+    private val rvTopHeadLineAdapter by lazy {
+        TopHeadlineAdapter { news ->
+            onTopHeadlineOnClick(news)
+        }
+    }
+
     override fun getViewModelClass(): Class<HomeViewModel> = HomeViewModel::class.java
 
     override fun getLayoutId(): Int = R.layout.fragment_home
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        setUpAdapter()
+    }
+
+    private fun setUpAdapter() {
+        with(viewBinding.rvTopHeadline) {
+            adapter = rvTopHeadLineAdapter
+        }
+    }
+
+    private fun onTopHeadlineOnClick(news: News) {
+        Toast.makeText(context, news.title, Toast.LENGTH_SHORT).show()
     }
 }
