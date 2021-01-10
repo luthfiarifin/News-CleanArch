b7b922fc58133fd343b0823031961fdd85a61226
Luthfi Arifin <luthfiarifin0222@gmail.com>
2021-01-10 18:39:24 +0700

    Create DataMapper.kt

---
+++ core/src/main/java/com/laam/core/utils/DataMapper.kt
@@ -0,0 +1,24 @@
+package com.laam.core.utils
+
+import com.laam.core.data.source.local.entity.NewsEntity
+import com.laam.core.data.source.remote.response.ArticleResponse
+import com.laam.core.domain.model.NewsDomain
+import com.laam.core.presentation.model.News
+
+/**
+ * Created by luthfiarifin on 1/10/2021.
+ */
+object DataMapper {
+
+    fun NewsEntity.mapToNewsDomain() = NewsDomain(
+        url, author, content, description, publishedAt, source, title, urlToImage
+    )
+
+    fun ArticleResponse.mapToNewsEntity() = NewsEntity(
+        url, author, content, description, publishedAt, source.name, title, urlToImage
+    )
+
+    fun NewsDomain.mapToNews() = News(
+        url, content, publishedAt, source, title, urlToImage
+    )
+}
