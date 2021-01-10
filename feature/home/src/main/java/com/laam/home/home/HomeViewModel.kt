17ea5c1dc5cf4bdaea3f36cee82f76efb71e09e7
Luthfi Arifin <luthfiarifin0222@gmail.com>
2021-01-10 19:27:05 +0700

    Configure get top headline data

--- feature/home/src/main/java/com/laam/home/home/HomeViewModel.kt
+++ feature/home/src/main/java/com/laam/home/home/HomeViewModel.kt
@@ -1,8 +1,16 @@
 package com.laam.home.home
 
+import androidx.hilt.lifecycle.ViewModelInject
+import androidx.lifecycle.asLiveData
 import com.laam.base.BaseViewModel
+import com.laam.core.domain.usecase.NewsUseCase
 
 /**
  * Created by luthfiarifin on 1/8/2021.
  */
-class HomeViewModel : BaseViewModel()
+class HomeViewModel @ViewModelInject constructor(
+    newsUseCase: NewsUseCase
+) : BaseViewModel() {
+
+    val newsTopHeadLine = newsUseCase.getTopHeadline().asLiveData()
+}
