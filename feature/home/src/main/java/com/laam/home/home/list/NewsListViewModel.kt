15733890d0683aaeb1139b6694376de1734bc238
Luthfi Arifin <luthfiarifin0222@gmail.com>
2021-01-11 00:49:26 +0700

    Create & configure NewsListFragment.kt

---
+++ feature/home/src/main/java/com/laam/home/home/list/NewsListViewModel.kt
@@ -0,0 +1,19 @@
+package com.laam.home.home.list
+
+import androidx.hilt.lifecycle.ViewModelInject
+import androidx.lifecycle.asLiveData
+import com.laam.base.BaseViewModel
+import com.laam.core.domain.usecase.NewsUseCase
+
+/**
+ * Created by luthfiarifin on 1/10/2021.
+ */
+class NewsListViewModel @ViewModelInject constructor(
+    private val newsUseCase: NewsUseCase
+) : BaseViewModel() {
+
+    var page: Int = 1
+    var category: String = ""
+
+    fun newsEverything() = newsUseCase.getSearch(page, category).asLiveData()
+}
