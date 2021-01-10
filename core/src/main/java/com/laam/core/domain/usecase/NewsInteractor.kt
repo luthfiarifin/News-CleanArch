118ccc8364faa4d6016cf8ec369cf03481f3ada6
Luthfi Arifin <luthfiarifin0222@gmail.com>
2021-01-10 18:38:19 +0700

    Configure NewsInteractor.kt

---
+++ core/src/main/java/com/laam/core/domain/usecase/NewsInteractor.kt
@@ -0,0 +1,20 @@
+package com.laam.core.domain.usecase
+
+import com.laam.core.data.Resource
+import com.laam.core.domain.model.NewsDomain
+import com.laam.core.domain.repository.INewsRepository
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+/**
+ * Created by luthfiarifin on 1/10/2021.
+ */
+class NewsInteractor @Inject constructor(private val newsRepository: INewsRepository) :
+    NewsUseCase {
+
+    override fun getTopHeadline(): Flow<Resource<List<NewsDomain>>> =
+        newsRepository.getTopHeadline()
+
+    override fun getSearch(page: Int, q: String?): Flow<Resource<List<NewsDomain>>> =
+        newsRepository.getSearch(page, q)
+}
---
+++ core/src/main/java/com/laam/core/domain/usecase/NewsUseCase.kt
@@ -0,0 +1,15 @@
+package com.laam.core.domain.usecase
+
+import com.laam.core.data.Resource
+import com.laam.core.domain.model.NewsDomain
+import kotlinx.coroutines.flow.Flow
+
+/**
+ * Created by luthfiarifin on 1/10/2021.
+ */
+interface NewsUseCase {
+
+    fun getTopHeadline(): Flow<Resource<List<NewsDomain>>>
+
+    fun getSearch(page: Int, q: String?): Flow<Resource<List<NewsDomain>>>
+}
