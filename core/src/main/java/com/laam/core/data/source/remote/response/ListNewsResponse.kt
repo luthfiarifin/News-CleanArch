c5ce96b123e19eeb0dd1285d64878b39b5d67e50
Luthfi Arifin <luthfiarifin0222@gmail.com>
2021-01-10 18:15:03 +0700

    Create news response

--- core/src/main/java/com/laam/core/data/source/remote/response/ListNewsResponse.kt
+++ core/src/main/java/com/laam/core/data/source/remote/response/ListNewsResponse.kt
@@ -6,7 +6,7 @@ import com.google.gson.annotations.SerializedName
  * Created by luthfiarifin on 1/7/2021.
  */
 data class ListNewsResponse(
-    @SerializedName("articles") val articles: List<ArticleResponse>,
-    @SerializedName("status") val status: String,
-    @SerializedName("totalResults") val totalResults: Int
+    @field:SerializedName("articles") val articles: List<ArticleResponse>,
+    @field:SerializedName("status") val status: String,
+    @field:SerializedName("totalResults") val totalResults: Int
 )
