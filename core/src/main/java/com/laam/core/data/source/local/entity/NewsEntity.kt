fb5e1aeab298cb4d603f128dd487557c3861d808
Luthfi Arifin <luthfiarifin0222@gmail.com>
2021-01-10 18:13:22 +0700

    Change NewsEntity.kt primary key

--- core/src/main/java/com/laam/core/data/source/local/entity/NewsEntity.kt
+++ core/src/main/java/com/laam/core/data/source/local/entity/NewsEntity.kt
@@ -10,16 +10,12 @@ import com.laam.core.data.source.local.room.NewsDatabase
  */
 @Entity(tableName = NewsDatabase.NEWS_COLUMN_NAME)
 data class NewsEntity(
-    @ColumnInfo(name = "author") var author: String,
+    @PrimaryKey @ColumnInfo(name = "url") var url: String,
+    @ColumnInfo(name = "author") var author: String?,
     @ColumnInfo(name = "content") var content: String?,
     @ColumnInfo(name = "description") var description: String,
     @ColumnInfo(name = "publishedAt") var publishedAt: String,
     @ColumnInfo(name = "source") var source: String,
     @ColumnInfo(name = "title") var title: String,
-    @ColumnInfo(name = "url") var url: String,
     @ColumnInfo(name = "urlToImage") var urlToImage: String
-) {
-
-    @PrimaryKey(autoGenerate = true)
-    @ColumnInfo(name = "id") var newsId: Long? = null
-}
+)
