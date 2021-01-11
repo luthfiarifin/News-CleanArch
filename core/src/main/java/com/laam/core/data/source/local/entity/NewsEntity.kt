d2dcc64e0120b28426b352c0eeaa16ee2e515093
Luthfi Arifin <luthfiarifin0222@gmail.com>
2021-01-11 13:37:47 +0700

    Add `qSearch` entity in NewsEntity.kt

--- core/src/main/java/com/laam/core/data/source/local/entity/NewsEntity.kt
+++ core/src/main/java/com/laam/core/data/source/local/entity/NewsEntity.kt
@@ -17,5 +17,6 @@ data class NewsEntity(
     @ColumnInfo(name = "publishedAt") var publishedAt: String,
     @ColumnInfo(name = "source") var source: String,
     @ColumnInfo(name = "title") var title: String,
-    @ColumnInfo(name = "urlToImage") var urlToImage: String
+    @ColumnInfo(name = "urlToImage") var urlToImage: String,
+    @ColumnInfo(name = "qSearch") var qSearch: String
 )
