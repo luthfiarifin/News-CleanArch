e44076a1cb20e575b9bb17e6b5b8e5ade60c67f7
Luthfi Arifin <luthfiarifin0222@gmail.com>
2021-01-15 21:07:58 +0700

    Create and configure NewsSearchFragment.kt

---
+++ feature/search/src/main/java/com/laam/search/NewsSearchFragment.kt
@@ -0,0 +1,123 @@
+package com.laam.search
+
+import android.os.Bundle
+import android.view.MenuItem
+import android.view.View
+import android.view.inputmethod.EditorInfo
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.laam.base.BaseFragment
+import com.laam.base.adapter.NewsListAdapter
+import com.laam.core.data.Resource
+import com.laam.core.presentation.model.News
+import com.laam.core.utils.DataMapper.mapToNews
+import com.laam.search.databinding.FragmentNewsSearchBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+/**
+ * Created by luthfiarifin on 1/12/2021.
+ */
+@AndroidEntryPoint
+class NewsSearchFragment : BaseFragment<FragmentNewsSearchBinding, NewsSearchViewModel>() {
+
+    override fun getViewModelClass(): Class<NewsSearchViewModel> = NewsSearchViewModel::class.java
+
+    override fun getLayoutId(): Int = R.layout.fragment_news_search
+
+    private val rvListAdapter by lazy {
+        NewsListAdapter { news ->
+            onNewsListClick(news)
+        }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setUpToolbar()
+        setUpBinding()
+        setUpAdapter()
+        setUpSearch()
+    }
+
+    private fun setUpToolbar() {
+        (activity as AppCompatActivity?)?.supportActionBar?.apply {
+            title = context?.getString(R.string.search)
+
+            setHasOptionsMenu(true)
+            setDisplayHomeAsUpEnabled(true)
+            setDisplayShowHomeEnabled(true)
+        }
+    }
+
+    private fun setUpSearch() {
+        viewBinding.searchView.setOnEditorActionListener { _, i, _ ->
+            if (i == EditorInfo.IME_ACTION_SEARCH) {
+                viewBinding.searchView.clearFocus()
+                onSearch()
+                true
+            } else false
+        }
+    }
+
+    private fun onSearch() {
+        viewModel.qSearch = viewBinding.searchView.text.toString()
+        observeNews()
+    }
+
+    private fun setUpBinding() {
+        viewBinding.viewModel = viewModel
+    }
+
+    private fun setUpAdapter() {
+        with(viewBinding.rvList) {
+            adapter = rvListAdapter
+        }
+    }
+
+    private fun observeNews() {
+        viewModel.newsEverything().observe(viewLifecycleOwner, { news ->
+            when (news) {
+                is Resource.Loading -> {
+                    viewModel.isLoading.set(true)
+                }
+                is Resource.Success -> {
+                    val newsList = news.data?.map { it.mapToNews() }
+                    rvListAdapter.submitList(newsList)
+                    startShimmer(false)
+                    viewModel.isLoading.set(false)
+                }
+                is Resource.Error -> {
+                    startShimmer(false)
+                    viewModel.isLoading.set(false)
+                }
+            }
+        })
+    }
+
+    private fun onNewsListClick(news: News) {
+        Toast.makeText(context, news.title, Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        startShimmer()
+    }
+
+    override fun onPause() {
+        startShimmer(false)
+        super.onPause()
+    }
+
+    private fun startShimmer(isStart: Boolean = true) {
+        if (isStart) viewBinding.placeHolderNewsList.shimmer.startShimmer()
+        else viewBinding.placeHolderNewsList.shimmer.stopShimmer()
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == android.R.id.home) {
+            activity?.onBackPressed()
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+}
