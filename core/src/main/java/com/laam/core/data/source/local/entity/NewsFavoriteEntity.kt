7ce94aacb678c6f0588ab64ad8a19ebab002408d
Luthfi Arifin <luthfiarifin0222@gmail.com>
2021-01-16 23:40:17 +0700

    Add NewsFavoriteEntity.kt

---
+++ core/src/main/java/com/laam/core/data/source/local/entity/NewsFavoriteEntity.kt
@@ -0,0 +1,22 @@
+package com.laam.core.data.source.local.entity
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.laam.core.data.source.local.room.NewsDatabase
+
+/**
+ * Created by luthfiarifin on 1/16/2021.
+ */
+@Entity(tableName = NewsDatabase.NEWS_FAVORITE_TABLE_NAME)
+data class NewsFavoriteEntity(
+    @PrimaryKey @ColumnInfo(name = "url") var url: String,
+    @ColumnInfo(name = "author") var author: String?,
+    @ColumnInfo(name = "content") var content: String?,
+    @ColumnInfo(name = "description") var description: String,
+    @ColumnInfo(name = "publishedAt") var publishedAt: String,
+    @ColumnInfo(name = "source") var source: String,
+    @ColumnInfo(name = "title") var title: String,
+    @ColumnInfo(name = "urlToImage") var urlToImage: String,
+    @ColumnInfo(name = "qSearch") var qSearch: String
+)
--- core/src/main/java/com/laam/core/data/source/local/room/NewsDatabase.kt
+++ core/src/main/java/com/laam/core/data/source/local/room/NewsDatabase.kt
@@ -3,17 +3,21 @@ package com.laam.core.data.source.local.room
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.laam.core.data.source.local.entity.NewsEntity
+import com.laam.core.data.source.local.entity.NewsFavoriteEntity
 
 /**
  * Created by luthfiarifin on 1/7/2021.
  */
-@Database(entities = [NewsEntity::class], version = 1, exportSchema = false)
+@Database(entities = [NewsEntity::class, NewsFavoriteEntity::class], version = 2, exportSchema = false)
 abstract class NewsDatabase : RoomDatabase() {
 
     abstract fun newsDao(): NewsDao
 
+    abstract fun newsFavoriteDao(): NewsFavoriteDao
+
     companion object {
         const val DB_NAME = "News.db"
         const val NEWS_TABLE_NAME = "news"
+        const val NEWS_FAVORITE_TABLE_NAME = "news_favorite"
     }
 }
