d1550aa5cb11c25c5ccd1e6747c96daf0e41d58a
Luthfi Arifin <luthfiarifin0222@gmail.com>
2021-01-18 01:38:12 +0700

    Configue DI for feature DFM

--- app/src/main/java/com/laam/news_cleanarch/di/AppModule.kt
+++ app/src/main/java/com/laam/news_cleanarch/di/AppModule.kt
@@ -5,13 +5,13 @@ import com.laam.core.domain.usecase.NewsUseCase
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
-import dagger.hilt.android.components.ActivityComponent
+import dagger.hilt.android.components.ApplicationComponent
 
 /**
  * Created by luthfiarifin on 1/10/2021.
  */
 @Module
-@InstallIn(ActivityComponent::class)
+@InstallIn(ApplicationComponent::class)
 abstract class AppModule {
 
     @Binds
