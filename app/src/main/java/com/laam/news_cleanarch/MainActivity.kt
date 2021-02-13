0962795fd816e42d116fc57a046499b56fa7f9fe
Luthfi Arifin <luthfiarifin0222@gmail.com>
2021-02-14 00:03:51 +0700

    Fix leak in MainActivity.kt

--- app/src/main/java/com/laam/news_cleanarch/MainActivity.kt
+++ app/src/main/java/com/laam/news_cleanarch/MainActivity.kt
@@ -12,12 +12,10 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var viewBinding: ActivityMainBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        viewBinding =
+        val viewBinding: ActivityMainBinding =
             DataBindingUtil.setContentView(this, R.layout.activity_main)
 
         setSupportActionBar(viewBinding.appToolbar.toolbar)
